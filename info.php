aec80b1f805ba4d3ee7ec4329537f409db429abe
root <servantarcherubw@gmail.com>
2016-11-13 13:45:57 -0600

    Cambio en la presentación

--- info.php
+++ info.php
@@ -8,7 +8,7 @@
     // insert data
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT name FROM Becarios WHERE nickname = '".$nickname."'";
+    $sql = "SELECT name,status FROM Becarios WHERE nickname = '".$nickname."'";
     $q = $pdo->prepare($sql);
     $q->execute();
     $data = $q->fetch(PDO::FETCH_ASSOC);
@@ -17,13 +17,19 @@
     }
     else {
         $name = $data['name'];
+        $status = $data['status'];
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "SELECT hora_entrada,hora_salida FROM HorarioBecario WHERE becario = '".$nickname."'";
 ?>
-<div class="col l10 offset-l1 m10 offset-m1 s12">
+<div class="col l10 offset-l1 m10 offset-m1 s12 center">
 <table class="highlight centered bordered">
-    <caption><b>Becario: </b><?php echo $name;?></caption>
+<caption>
+    <ul>
+    <li><b>Becario: </b><?php echo $name;?></li>
+    <li><b>Estado: </b><?php echo $status;?></li>
+</ul>
+</caption>
     <thead class="blue lighten-2 white-text">
         <tr>
             <th>Entrada</th>
